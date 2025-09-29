8f6958d0b3234c6e67f176e5629314769052b52d
Carlo Amezcua <carlo.141592@gmail.com>
2025-09-28 23:52:51 -0600

    Problem 9 was addressed.

--- Problem_9.py
+++ Problem_9.py
@@ -1,2 +1,12 @@
-a = sqrt(1000)
-print(a)
+def factorial(n):
+    n_factorial = 1
+    for i in range(1, n+1):
+        n_factorial = n_factorial * i
+    return n_factorial
+
+def combinatoria (n, k):
+    combinatoria = factorial(n) / (factorial(k) * factorial(n - k))
+    return int(combinatoria)
+
+a = combinatoria(9, 3)
+a
