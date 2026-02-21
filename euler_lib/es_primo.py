4349fce3ff4e3fc6f22710a64435cf4cc1a7e754
Carlo Amezcua <carlo.141592@gmail.com>
2026-02-21 14:48:20 -0600

    fix: correct prime number check logic in es_primo function

--- euler_lib/es_primo.py
+++ euler_lib/es_primo.py
@@ -1,7 +1,9 @@
 def es_primo(n):
-    if n < 2:
+    if n == 2:
+        return True
+    elif n < 2 or n % 2 == 0:
         return False
-    for i in range(2, int(n**0.5) + 1):
+    for i in range(3, int(n**0.5) + 1, 2):
         if n % i == 0:
             return False
     return True
